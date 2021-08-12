f98812d32b88283f46c7bd29ef8709a7cea62888
Arthedain420D <88175049+Arthedain420D@users.noreply.github.com>
2021-08-12 17:17:03 +0800

    Add files via upload

--- med2image.py
+++ med2image.py
@@ -5,3 +5,19 @@ import med2image
 
 inputFile = r"E:\Thesis\Images\DICOM"
 outputDir = 'E:\Thesis\Images\PNG'
+
+med2image -inputFile -outputDir
+
+# =============================================================================
+# # Convert a DICOM file 'file.dcm' to JPEG and store
+# # the results in a dirctory called 'out'.
+# # The 'out' dir will contain a set of JPEG
+# # images of form 'output-sliceXXX.jpg'.
+# 
+# # NOTE! If the directory containing 'file.dcm' contains
+# # multiple DICOM files, *ALL* of these will be converted
+# # to JPEG. See later for only converting a *single*
+# # DICOM file.
+# 
+# med2image -i file.dcm -d out
+# =============================================================================
